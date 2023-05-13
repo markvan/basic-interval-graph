4aa92328d1cb0287e928375756ee9715983f8424
markvan <markvanharmelen@gmail.com>
2023-05-13 10:18:16 +0100

    made InputParserTest.parseFile test comprehensively, also removed JUnit4 test package this commit or previous

--- src/IntervalGraphTest/InputParserTest.java
+++ src/IntervalGraphTest/InputParserTest.java
@@ -8,7 +8,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -33,23 +33,37 @@ class InputParserTest {
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
         File file = new File("InputParserTestData.txt");
-        if (file.delete()) {
-            // System.out.println("Successfully tore down test data by deleting 'InputParserTestData.txt'");
-        } else {
+        if ( ! file.delete()) {
             System.out.println("Failed to delete InputParserTestData.txt.");
         }
     }
 
     @Test
     void parseFile() {
-        ArrayList<Interval> expectedIntervals= new ArrayList<Interval>();
+        ArrayList<Interval> expectedIntervals= new ArrayList<>();  // had a warning with explicit type in initialisation
         expectedIntervals.add(new Interval("A", 1, 2));
         expectedIntervals.add(new Interval("B", 3, 4));
 
         ArrayList<Interval> sutReturnValue = new InputParser("InputParserTestData.txt").parseFile();
+
+        // test for SUT correctness
+
+        // illustrative test without hamcrest
         assertEquals(sutReturnValue, expectedIntervals);
         // better style with hamcrest
+        assertThat(sutReturnValue, is(equalTo(expectedIntervals)));   // fails with not
 
+        //test several cases that should not match SUT
+
+        //unequal length lists, same initial Intervals in expected, fails without not
+        assertThat(sutReturnValue, is(not(equalTo(expectedIntervals.add(new Interval("extra interval", 3, 4))))));   // fails with not
+        //another unequal length test, empty list expected, fails without not
+        assertThat(sutReturnValue, is(not(equalTo(new ArrayList<Interval>() ))));
+        //test if fails same length but different interval
+        ArrayList<Interval> expectedIntervalsWithDiffNode= new ArrayList<>();
+        expectedIntervalsWithDiffNode.add(new Interval("A", 1, 2));
+        expectedIntervalsWithDiffNode.add(new Interval("B", 3, 6)); // this node has different end value
+        assertThat(sutReturnValue, is(not(equalTo(expectedIntervalsWithDiffNode))));   // fails without not
 
     }
 }
