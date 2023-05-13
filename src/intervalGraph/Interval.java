5c9db2d90e5bf3a3300c52c3d9bbf5944eef418a
Mark van Harmelen <mark@Marks-Mac-mini.local>
2023-05-13 07:55:22 +0100

    added instance vars and constructor to Interval

--- src/intervalGraph/Interval.java
+++ src/intervalGraph/Interval.java
@@ -1,4 +1,16 @@
 package intervalGraph;
 
+// class to contain an interval used for input from file
+
 public class Interval {
+    final String name;
+    final int start;
+    final int end;
+
+    public Interval(String n, int s, int e){
+        name = n;
+        start = s;
+        end = e;
+    }
+
 }
