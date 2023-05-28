3d48add782b8851905799fd1a766c5ac1829b841
markvan <markvanharmelen@gmail.com>
2023-05-28 13:13:24 +0100

    javadoc for IntervalGraph

--- src/intervalGraph/AdjacencyList.java
+++ src/intervalGraph/AdjacencyList.java
@@ -2,16 +2,18 @@ package intervalGraph;
 
 import java.util.*;
 
-// The AdjacencyList class implements an interval graph
-// in a more sophisticated approach where one could choose between implementations
-// IntervalGraph might be an (abstract) interface with AdjacencyList as one of the implementations
+/** The AdjacencyList class implements an interval graph<br><br>
+ *
+ *In a more sophisticated approach where one could choose between implementations
+ * IntervalGraph might be an (abstract) interface with AdjacencyList as one of the implementations
+*/
 
 public class AdjacencyList {
     HashMap<Interval, List<Interval>> adjacencyLists = new HashMap<>();
 
     /**
      * constructor for an AdjacencyList, which is an implementation of an interval graph
-     * @param intervalList is a list of intervals forming the interval graph - List&lt;Interval&gt;
+     * @param intervalList  a list of intervals forming the interval graph - List&lt;Interval&gt;
      */
     public AdjacencyList(List<Interval> intervalList) {
         // make one adjacency lists for each interval in the Hashmap adjacencyLists
@@ -32,12 +34,22 @@ public class AdjacencyList {
         }
     }
 
-    // get the interval names from the nodes in the interval graph
+    /**
+     * getIntervalNames - get the names of the intervals in the interval graph
+     *
+     * @return a Set&lt;String&gt; set of interval names, sorted
+     */
     public Set<String> getIntervalNames() {
         Set<String> retSet = new TreeSet<>();
         (adjacencyLists.keySet()).forEach((i) -> retSet.add(i.getName()));
         return retSet;
     }
+
+    /**
+     * getIntervalNames - get the intervals in the interval graph
+     *
+     * @return a Set&lt;Interval&gt; set of interval names, sorted
+     */
     public Set<Interval> getIntervals() {
         return adjacencyLists.keySet();
     }
@@ -45,7 +57,7 @@ public class AdjacencyList {
     /**
      * getOverlappingtervals returns a list of intervals that overlap the named interval
      * @param  name of the interval whose overlapping intervals are wanted - String
-     * @return list of intervals that overlap the named interval - List<Interval>
+     * @return list of Intervals that overlap the named interval
      * @throws Exception if the named interval is not found - todo create subclass of Exception for this
      */
     public List<Interval> getOverlappingtervals(String name) throws Exception {
@@ -72,17 +84,27 @@ public class AdjacencyList {
         return retOverlappingIntervalsList;
     }
 
+    /** intervalsOverlap returns a bool to indicate if two intervals overlap
+     * @param interval1 first interval
+     * @param interval2 second interval
+     * @return true or false depending on if overlap is found
+     */
     //not crazy about this being public but need to test it
     static public boolean intervalsOverlap(Interval interval1, Interval interval2) {
         return ( interval1.getEnd() > interval2.getStart() && interval1.getStart() < interval2.getEnd() );
     }
 
+    /**
+     * @param intervalName - name of the interval wanted
+     * @return the interval with the name, null if there is no interval with that name
+     */
     public Interval getIntervalFromName(String intervalName) {
         Set<Interval> intervals = getIntervals();
         for (Interval i : intervals) {
             if (intervalName.equals(i.getName())) {
                 return i;
             }
+            return null;
         }
         return null;
     }
